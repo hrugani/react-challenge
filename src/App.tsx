f60ff944c0574007cd4c6a2d5bdc66fa4b7d46a4
helio rugani brandao <hrugani@gmail.com>
2020-12-19 22:25:00 -0300

    changed main page - The content of the page now has only a AntD table-view-compoenet that executes a graphql query for getting data about currencies. This change is only to get a similar example. In the next steps this component will be adapted to getting the right infomation the assignment ask for.

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Table } from "antd";
+import { useQuery, gql } from "@apollo/client";
+
+const EXCHANGE_RATES = gql`
+  query GetExchangeRates {
+    rates(currency: "USD") {
+      currency
+      rate
+      name
+    }
+  }
+`;
+
+const columns = [
+  {
+    title: "Currency",
+    dataIndex: "currency",
+    key: "currency",
+    render: (text) => (
+      <a href={`https://www.x-rates.com/table/?from=${text}&amount=1`}>
+        {text}
+      </a>
+    )
+  },
+  {
+    title: "Name",
+    dataIndex: "name",
+    key: "name"
+  },
+  {
+    title: "Rate",
+    dataIndex: "rate",
+    key: "rate"
+  }
+];
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const { loading, error, data } = useQuery(EXCHANGE_RATES);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
+  return <Table columns={columns} dataSource={data.rates} rowKey="currency" />;
 }
 
-
-
 export default App;
-
-
